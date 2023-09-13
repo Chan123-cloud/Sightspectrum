cfc77f8647cec4533641ec7f58763f58ee4fc04c
Chan123-cloud <mchandrakumarms@gmail.com>
2023-09-13 12:57:28 +0530

    first commit

---
+++ week2task3/Chan.java
@@ -0,0 +1,6 @@
+package MS;
+public class Chan {
+	int a=10;
+	int b=20;
+
+}
---
+++ week2task3/Hari.java
@@ -0,0 +1,13 @@
+package MS;
+
+public class Hari {
+
+	public static void main(String[] args) {
+		Chan c=new Chan();
+		System.out.println(c.a);
+		System.out.println(c.b);
+		
+
+	}
+
+}
