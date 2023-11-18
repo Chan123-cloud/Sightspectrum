99fc013b61ac15a91b56312c2b2bbe7653d6ebb6
Chan123-cloud <mchandrakumarms@gmail.com>
2023-11-18 17:11:24 +0530

    first commit

---
+++ Java/src/JavaPrograms/SwappingOfTwoNumbers.java
@@ -0,0 +1,24 @@
+package JavaPrograms;
+
+public class SwappingOfTwoNumbers {
+	
+	public static void main (String [] args) {
+
+		int a = 10 , b = 20 ;
+
+		a = a + b ;
+
+		b = a - b ;
+
+		a = a - b ;
+
+		System.out.println(a);
+
+		System.out.print(b);
+
+		}
+
+		}
+		 
+
+	
